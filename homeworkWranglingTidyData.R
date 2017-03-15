c03f5a90242ed8fd19185d3ceab719ca4994537b
bsevansunc <bsevans.unc@gmail.com>
2017-03-15 10:13:25 -0400

    added semi-clean capture and birdTables

--- homeworkWranglingTidyData.R
+++ homeworkWranglingTidyData.R
@@ -110,13 +110,14 @@ visitTable <- readWorkshopData(gitSite, 'visitTable')
 #=================================================================================*
 
 # Instructions: 
+#
 # a) For each of the following tasks, use tidyverse (tidyr and
 #    dplyr) functions. 
 # b) Below each task prompt, provide the code used to complete the task. There is
 #    no need to provide an answer but the output of the code must directly solve
 #    the task (see examples below).
-# c) Unless asked to do so, do not assign names to output tables (i.e., each
-#    task must begin with the raw data). 
+# c) In completing this assignment, do not assign names to any object (either 
+#    output or intermediate steps) -- rather, use piping to connect steps.
 
 #---------------------------------------------------------------------------------*
 # ---- EXAMPLES ---
@@ -133,12 +134,14 @@ birdTable %>%
   filter(species == 'NOCA') %>%
   nrow
 
-# Example 3. Subset captureTable to only the fields birdID, visitID, age, and sex:
+# Example 3. Subset captureTable to only the fields birdID, visitID, age, and
+# sex:
 
 captureTable %>%
   select(birdID, visitID, age, sex)
 
-# Example 4. Join birdTable to a subset of captureTable that includes only the columns birdID, visitID, age, and sex:
+# Example 4. Join birdTable to a subset of captureTable that includes only the
+# columns birdID, visitID, age, and sex:
 
 birdTable %>%
   left_join(
@@ -153,92 +156,60 @@ birdTable %>%
 
 # 1. Subset the siteIdTable to only sites from the region 'Springfield':
 
-siteIdTable %>%
-  filter(region == 'Springfield')
 
 # 2. Subset the siteIdTable to only the fields siteID and region:
 
-siteIdTable %>%
-  select(siteID, region)
+
 
 # 3. Subset the siteLocation Table to only locations in Amherst, Massachussetts:
 
-siteLocationTable %>% 
-  filter(city == 'Amherst')
 
 # 4. Subset the siteLocationTable to only locations in Monson, Amherst, and
 # Belchertown Massachussetts:
 
-siteLocationTable %>% 
-  filter(city %in% c('Monson', 'Amherst','Belchertown'))
 
 # 5. Repeat #4 and subset the resultant frame to the fields siteID, houseNumber,
 # street, city, state, and zip:
 
-siteLocationTable %>% 
-  filter(city %in% c('Monson', 'Amherst','Belchertown')) %>%
-  select(siteID, houseNumber:zip)
 
 # 6. Using the functions, select, filter, and distinct subset the
 # siteLocationTable such that the output is a data table of unique cities
 # studied in  Massachussets:
 
-siteLocationTable %>%
-  select(city, state) %>%
-  filter(state == 'MA') %>%
-  distinct
+
 
 # 7. The function 'year()' can be used to extract the year from a date object.
 # Subset the visitTable to only visits from 2015:
 
-visitTable %>%
-  filter(year(dateVisit) == 2015)
+
 
 # 8. Subset the visitTable to visitID, siteID, and dateVisit, and use 'left_join'
 # to join the resultant table to the siteIdentifierTable by "siteID":
 
-visitTable %>%
-  select(visitID, siteID, dateVisit) %>%
-  left_join(siteIdTable, by = 'siteID')
+
 
 # 9. Subset the visitTable to visitID, siteID, and dateVisit, and use 
 # 'left_join' to join the resultant table to the siteIdentifierTable by
 # "siteID", then subset the columns to visitID, siteID, dateVisit, and region:
 
-visitTable %>%
-  select(visitID, siteID, dateVisit) %>%
-  left_join(siteIdTable, by = 'siteID') %>%
-  select(visitID, siteID, dateVisit, region)
+
 
 # 10. Subset the visitTable to visitID, siteID, and dateVisit, and use 
 # 'left_join' to join the resultant table to the siteIdentifierTable by
 # "siteID", subset the columns to visitID, siteID, dateVisit, and region, then
 # filter to only visits in the Springfield region in 2013:
 
-visitTable %>%
-  select(visitID, siteID, dateVisit) %>%
-  left_join(siteIdTable, by = 'siteID') %>%
-  select(visitID, siteID, dateVisit, region) %>%
-  filter(year(dateVisit) == 2013, region == 'Springfield')
+
 
 # 11. Calculate how many visits there were in the DC region in 2008:
 
-visitTable %>%
-  select(visitID, siteID, dateVisit) %>%
-  left_join(siteIdTable, by = 'siteID') %>%
-  select(visitID, siteID, dateVisit, region) %>%
-  filter(year(dateVisit) == 2008, region == 'DC') %>%
-  select(visitID) %>%
-  distinct %>%
-  nrow
+
 
 # 12. Use the birdTable with group_by and summarize functions calculate how many
 # birds of each species have been banded by Neighborhood Nestwatch. Name the
 # summary column "tSpecies":
 
-birdTable %>%
-  group_by(species) %>%
-  summarize(tSpecies = n())
+
 
 # 13. There's a lot of extraneous species in the results from #12. Use my
 # focal species vector below to subset the birdTable to just our species of
@@ -248,157 +219,61 @@ birdTable %>%
 focalSpecies <- c('AMRO', 'BCCH', 'BRTH', 'CACH', 'CARW', 'GRCA', 'HOWR',
                   'NOCA', 'NOMO', 'SOSP', 'TUTI')
 
-birdTable %>%
-  filter(species %in% focalSpecies) %>%
-  group_by(species) %>%
-  summarize(tSpecies = n())
+
 
 # 14. Subset the birdTable to the columns birdID, species, colorComboL, and
 # colorComboR. Join a subset of the capture table that includes the columns
 # birdID, visitID, typeCapture, age, and sex, by the column "birdID". 
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID')
+
 
 # 15. Repeat #14 and use the resultant table to calculate the number of male (M)
 # and female (F) NOCA that have been banded (typeCapture == 'B') by Neighborhood
 # Nestwatch:
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  filter(species == 'NOCA') %>%
-  filter(typeCapture == 'B') %>%
-  group_by(sex) %>%
-  summarize(tCaptures = n()) %>%
-  filter(sex %in% c('M', 'F'))
+
 
 # 16. Repeat #14 and join to the visitTable, subset to the columns visitID,
 # siteID, and dateVisit, by 'visitID':
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  left_join(visitTable %>%
-              select(visitID, siteID, dateVisit), by = 'visitID')
+
 
 # 17. Repeat #16 and calculate the number of birds of each focal species that
 # were banded of each sex in 2016:
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  left_join(visitTable %>%
-              select(visitID, siteID, dateVisit), by = 'visitID') %>%
-  filter(year(dateVisit) == 2016) %>%
-  filter(typeCapture == 'B') %>%
-  filter(species %in% focalSpecies) %>%
-  group_by(species) %>%
-  summarize(tBanded = n())
+
 
 # 18. Repeat #16 and join the resultant table with the siteIdTable by the column
 # "siteID", subset to the siteID and region columns.
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  left_join(visitTable %>%
-              select(visitID, siteID, dateVisit), by = 'visitID') %>%
-  left_join(siteIdTable %>%
-              select(siteID, region), by = 'siteID')
+
 
 # 19. Calculate how many male and female NOCA were banded in the Pittsburgh region in 2016:
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  left_join(visitTable %>%
-              select(visitID, siteID, dateVisit), by = 'visitID') %>%
-  left_join(siteIdTable %>%
-              select(siteID, region), by = 'siteID') %>%
-  filter(region == 'Pittsburgh',
-         year(dateVisit) == 2015,
-         species == 'NOCA',
-         typeCapture == 'B') %>%
-  group_by(sex) %>%
-  summarize(tBanded = n()) %>%
-  filter(sex %in% c('F', 'M'))
+
 
 # 20. Calculate how many Gray Catbirds (GRCA) were banded (typeCapture is "B") in the DC region with
 # the alumimum (X) on the left and yellow (Y) over red (R) on the right leg:
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  left_join(visitTable %>%
-              select(visitID, siteID, dateVisit), by = 'visitID') %>%
-  left_join(siteIdTable %>%
-              select(siteID, region), by = 'siteID') %>%
-  distinct %>%
-  filter(region == 'DC',
-         species == 'GRCA',
-         typeCapture == 'B',
-         colorComboL == 'X', colorComboR == 'YR') %>%
-  nrow()
+
 
 # 21. Subset #18 above to female NOCA that were banded in the Springfield region
 # with a purple (M) band on the left leg:
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  left_join(visitTable %>%
-              select(visitID, siteID, dateVisit), by = 'visitID') %>%
-  left_join(siteIdTable %>%
-              select(siteID, region), by = 'siteID') %>%
-  filter(region == 'Springfield',
-         species == 'NOCA', 
-         sex == 'F',
-         str_detect(colorComboL, 'M'))
 
 # 22. A Gray Catbird was observed at Susannah Lerman's site ("LERMSUSMA1"), but 
 # the observer only saw one leg, which had alumimum over purple (XM), and they 
 # weren't sure if it was the left or right leg. Determine which bird or birds
 # this observation could be.
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  left_join(visitTable %>%
-              select(visitID, siteID, dateVisit), by = 'visitID') %>%
-  left_join(siteIdTable %>%
-              select(siteID, region), by = 'siteID') %>%
-  filter(
-    siteID == 'LERMSUSMA1',
-    species == 'GRCA',
-    str_detect(colorComboR, 'XM')|str_detect(colorComboL, 'XM'))
+
 
 # 23. The function getSiteDistanceFrame generates a data table of sites and 
 # their distances (in meters) from a target site. Below I generate a data frame
 # of site distance from Pete Marra's house (MARRPETMD2). Subset the output to
 # sites that are within 1km from Pete's house:
 
-getSiteDistanceFrame('MARRPETMD2') %>%
-  filter(siteDist < 1000)
+getSiteDistanceFrame('MARRPETMD2')
   
 # 24. Below I used the sitesByDistance function to calculate a vector of sites
 # within 2 km of Susannah Lerman's first Nestwatch site, LERMSUSMA1. Susannah
@@ -408,39 +283,12 @@ getSiteDistanceFrame('MARRPETMD2') %>%
 
 sitesByDistance('LERMSUSMA1', 2000)
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  left_join(visitTable %>%
-              select(visitID, siteID, dateVisit), by = 'visitID') %>%
-  left_join(siteIdTable %>%
-              select(siteID, region), by = 'siteID') %>%
-  filter(siteID %in% sitesByDistance('LERMSUSMA1', 2000),
-         species == 'GRCA',
-         str_detect(colorComboL, 'XG'),
-         str_detect(colorComboR, 'R'))
 
 # 25. Use the siteByDistance function to calculate the number of birds of each 
 # focal species that were not banded at Pete's site but were banded within 1 km
 # of his house:
 
-birdTable %>%
-  select(birdID, species, colorComboL, colorComboR) %>%
-  left_join(captureTable %>%
-              select(birdID, visitID, typeCapture, age, sex),
-            by = 'birdID') %>%
-  left_join(visitTable %>%
-              select(visitID, siteID, dateVisit), by = 'visitID') %>%
-  left_join(siteIdTable %>%
-              select(siteID, region), by = 'siteID') %>%
-  filter(siteID %in% sitesByDistance("MARRPETMD2", 1000),
-         siteID != 'MARRPETMD2',
-         species %in% focalSpecies) %>%
-  filter(typeCapture == 'B') %>%
-  group_by(species) %>%
-  summarize(tBanded= n())
+
 
          
 
