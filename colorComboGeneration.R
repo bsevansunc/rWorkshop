8ff8c7424396f8bc8e9bdeaef5e6f629415521d2
bsevansunc <bsevans.unc@gmail.com>
2017-03-18 09:54:04 -0400

    added a date buffer function and lots of comments

--- colorComboGeneration.R
+++ colorComboGeneration.R
@@ -1,9 +1,42 @@
 # Script to generate color combinations.
+
+#===============================================================================*
+# ---- SET UP ----
+#===============================================================================*
+
+# Smart installer will check list of packages that are installed, install any
+# necessary package that is missing, and load the library:
+
+smartInstallAndLoad <- function(packageVector){
+  for(i in 1:length(packageVector)){
+    package <- packageVector[i]
+    if(!package %in% rownames(installed.packages())){
+      install.packages(packageVector[i],repos="http://cran.rstudio.com/",
+                       dependencies=TRUE)
+    }
+  }
+  lapply(packageVector, library, character.only = TRUE)
+}
+
+# Load and potentially install libraries:
+
+smartInstallAndLoad(c('tidyverse', 'stringr', 'lubridate'))
+
 #===============================================================================*
 # ---- FUNCTIONS ----
 #===============================================================================*
 
-# Function to generate color combos:
+# Function to generate color combinations:
+# Inputs are:
+# - colorVector: A vector of colors (does not include aluminum)
+# - nBands: The number of total bands (including aluminum)
+# - xPositions: The location of the aluminum band (defaults to any):
+#     * 1 = Left leg, top
+#     * 2 = Left leg, bottom
+#     * 3 = Right leg, top
+#     * 4 = Right leg, bottom
+# - aluminum: TRUE/FALSE (defaults to TRUE), whether you want combinations
+#      that include an aluminum band
 
 getColorCombos <- function(colorVector, nBands, 
                            xPositions = 1:4, aluminum = TRUE){
@@ -27,7 +60,12 @@ getColorCombos <- function(colorVector, nBands,
   )
 }
 
-# Function to return a data frame of color combos, given a data frame of used combos:
+# Function to return a data frame of color combos, given a data frame of used
+# combos. Inputs include:
+# - colorComboFrame: Generated color combination data frame from output of
+#   getColorCombos, with the columns colorL and colorR
+# - previousComboFrame: Data frame of used color combinations with the
+#   columns colorL and colorR
 
 getUnusedCombos <- function(colorComboFrame, previousComboFrame){
   newCombos <- colorComboFrame %>%
@@ -37,6 +75,27 @@ getUnusedCombos <- function(colorComboFrame, previousComboFrame){
     )
   return(
     sample_n(newCombos, nrow(newCombos))
+  )
+}
+
+# Function to return a data frame of color combos, given a data frame of combos
+# used and the last date in which a bird was observed. Inputs include:
+# - colorComboFrame: Generated color combination data frame from output of
+#   getColorCombos, with the columns colorL and colorR
+# - comboDateFrame: Data frame of used color combinations with the
+#   columns colorL and colorR and a date column (ISO 8601) with the last
+#   date a bird was observed
+# - yearBuffer: The number of years in which a color combination should be
+#   exclude. This buffer is designed as "less than or equal to" so if, for
+#   example, the year buffer is 3, the output would exlude any combination
+#   used in the last 3 years.
+
+getCombosByYearBuffer <- function(colorComboFrame, comboDateFrame, yearBuffer){
+  currentYear <- year(Sys.Date())
+  combosToExclude <- comboDateFrame %>%
+    filter(currentYear - year(date) <= yearBuffer)
+  return(
+    getUnusedCombos(colorComboFrame, combosToExclude)
   )
 }
 
@@ -64,7 +123,15 @@ getColorCombos(jayColors, nBands = 3, xPositions = c(2,4))
 
 getColorCombos(jayColors, nBands = 3)
 
-# Perhaps we have a data frame of used combos
+#-------------------------------------------------------------------------------*
+# ---- TRYING IT OUT: UNUSED COMBOS ----
+#-------------------------------------------------------------------------------*
+
+# Let's use our jay combinations:
+
+jayCombos <- getColorCombos(jayColors, nBands = 4, xPositions = c(2,4))
+
+# And make a fake data frame of used combos:
 
 usedCombos <- jayCombos %>%
   sample_n(100)
@@ -80,6 +147,41 @@ nrow(jayCombos)
 nrow(usedCombos)
 
 nrow(newCombos)
+
+#-------------------------------------------------------------------------------*
+# ---- TRYING IT OUT: NEW COMBOS AFTER A GIVEN NUMBER OF YEARS ----
+#-------------------------------------------------------------------------------*
+
+# Let's use our jay combinations again:
+
+jayCombos <- getColorCombos(jayColors, nBands = 4, xPositions = c(2,4))
+
+# I'm going to add a column of fake dates that represent the last day the jay
+# was observed:
+
+jayCombosDate <- jayCombos %>%
+  mutate(date = sample(seq(as.Date('2005-01-01'),
+                           Sys.Date(), by="day"), nrow(jayCombos))
+)
+
+# Now we can simply run the function to determine the combos that can be
+# used:
+
+getCombosByYearBuffer(jayCombos, jayCombosDate, yearBuffer = 5)
+
+# Did it work? Let's check:
+
+nrow(jayCombos)
+
+jayCombosDate %>%
+  filter(year(Sys.Date()) - year(date) > 5) %>%
+  nrow
+
+getCombosByYearBuffer(jayCombos, jayCombosDate, yearBuffer = 5) %>%
+  nrow
+
+# Yup!
+
 
 
 
